e8b8900ae29cf1144a03a59ca446b14ac8c7569f
DionKanhai <dionkanhais@gmail.com>
2022-09-07 06:02:11 +0000

    add a method for the client upon successful connection and a username to the client upon connection to server

--- client.js
+++ client.js
@@ -12,11 +12,20 @@ const connect = function () {
     host: 'localhost', 
     port: 50541
   });
+
+  // use a connect handler to log message when connected to server
+  conn.on('connect', (connected) => {
+    console.log('Successfully connected to the game server', connected);
+    // upon connection adds this name to the server side
+    conn.write('Name: DKS');
+  });
   
+
   //event handler that receives data from the server to the client
   conn.on('data', (data) => {
     console.log('Server says: ', data)
   });
+
   // interpret incoming data as text
   conn.setEncoding('utf8');
 
