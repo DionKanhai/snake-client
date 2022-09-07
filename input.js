9179a4c3899bfd397b0f2b06bb14f748f7931a68
DionKanhai <dionkanhais@gmail.com>
2022-09-07 21:29:08 +0000

    set up a connection between client and server, and enable user to input keyboard commands to move the snake

--- input.js
+++ input.js
@@ -1,23 +1,52 @@
 // setup interface to handle user input from stdin (standard input)
 // allows us to listen for keyboard input and react to it
+const { stdin } = require("process");
 
-const setupInput = function () {
+// Stores the active TCP connection object
+let connection;
+
+const setupInput = function (conn) {
+  // ref the conn object 
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   // event listener 
   stdin.on('data', handleUserInput);
-  
+  // stdin.on('data', handleUserInput);
+  // complete version of the callback above
+  // stdin.on('data', (key) => {
+  //   handleUserInput(key)
+  // })
   return stdin;
 };
 
-// terminate the game when ctrl+c is entered by user
+// keyboard user input
 const handleUserInput = function (key) {
     if (key === '\u0003') {
       process.exit();
-    } 
+    };
+
+    if (key === 'w' || key === 'W') {
+      connection.write('Move: up')
+    }; 
+
+    if (key === 'a' || key === 'A') {
+      connection.write('Move: left')
+    };
+    
+    if (key === 's' || key === 'S') {
+      connection.write('Move: down')
+    }; 
+
+    if (key === 'd' || key === 'D') {
+      connection.write('Move: right')
+    }; 
 };
+
+
 
 // export variable
 module.exports = {
@@ -25,3 +54,4 @@ module.exports = {
 setupInput
 
 };
+
