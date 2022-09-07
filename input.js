e75e394dc619a4f526b4a053fe10bd86e02b49ac
DionKanhai <dionkanhais@gmail.com>
2022-09-07 23:20:53 +0000

    refactor file to move code into a separate file containing constants for user input and server connection

--- input.js
+++ input.js
@@ -1,6 +1,7 @@
 // setup interface to handle user input from stdin (standard input)
 // allows us to listen for keyboard input and react to it
 const { stdin } = require("process");
+const { INPUTKEYS } = require('./constants');
 
 // Stores the active TCP connection object
 let connection;
@@ -28,28 +29,12 @@ const handleUserInput = function (key) {
     if (key === '\u0003') {
       process.exit();
     };
-
-    if (key === 'w' || key === 'W') {
-      connection.write('Move: up')
-    }; 
-
-    if (key === 'a' || key === 'A') {
-      connection.write('Move: left')
-    };
-    
-    if (key === 's' || key === 'S') {
-      connection.write('Move: down')
-    }; 
-
-    if (key === 'd' || key === 'D') {
-      connection.write('Move: right')
-    };
-    // Display message next to snake in server
-    if (key === '#') {
-      connection.write('Say: NOO')
+     // if key is in inputkey object then send to server   
+    if (INPUTKEYS[key]) {
+      const command = INPUTKEYS[key]
+      connection.write(command)
     }
 };
-
 
 // export variable
 module.exports = {
