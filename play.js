da13677ef1aeab95e143685145fc1429bd66fed3
DionKanhai <dionkanhais@gmail.com>
2022-09-07 05:24:51 +0000

    refactor the file to import connect function

--- play.js
+++ play.js
@@ -1,28 +1,9 @@
 // import Node's net lib 
 const net = require('net');
+// use ES6 shorthand syntax to import object containing 
+// connect function in client file
+const { connect } = require('./client');
 
-// establishes a connection with the game server
-// used createConnection (found in the net lib) to create 
-// an object named conn 
 
-// the conn object represents the CONNECTION you have
-// with the server
-const connect = function () {
-  const conn = net.createConnection({
-    host: 'localhost', 
-    port: 50541
-  });
-  
-  //event handler that receives data from the server to the client
-  conn.on('data', (data) => {
-    console.log('Server says: ', data)
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-
-  return conn;
-};
-
-console.log('Connecting ...');
 connect();
 
