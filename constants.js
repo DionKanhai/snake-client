f63cb085ef204af0163a7997a07752657e6016e9
DionKanhai <dionkanhais@gmail.com>
2022-09-07 23:19:59 +0000

    add a file with the constants from the input file"

---
+++ constants.js
@@ -0,0 +1,19 @@
+// variables to store 
+const IP = 'localhost'
+const PORT = 50541
+
+// object for user input keys
+const INPUTKEYS = {
+  'w': 'Move: up',
+  'a': 'Move: left',
+  's': 'Move down', 
+  'd': 'Move right',
+  '#': 'Say: NOO'
+}
+
+module.exports = {
+  IP, 
+  PORT, 
+  INPUTKEYS
+};
+
